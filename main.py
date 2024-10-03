36b4e15ab1a17f60e211091ac9c713e597d0e72d
efftea <daanikwork@mail.ru>
2024-10-04 02:46:44 +0300

    Исправлено выделение момента в квадрат.

--- main.py
+++ main.py
@@ -29,9 +29,10 @@ while True:
     if not ret:
         print("Не удалось получить кадр")
         break
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    frame = cv2.inRange(frame, (0, 100, 100), (30, 255, 255))
-    cv2.imshow('Rec', frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv, (0, 100, 100), (30, 255, 255))
+    red_only = cv2.bitwise_and(frame, frame, mask=red_mask)
+    cv2.imshow('Rec', red_only)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
@@ -50,23 +51,29 @@ while True:
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     red_frame = cv2.inRange(hsv_frame, (0, 100, 100), (30, 255, 255))
+
     kernel = np.ones((5, 5), np.uint8)
-    erode = cv2.erode(red_frame, kernel, iterations=1)
-    dilate = cv2.dilate(erode, kernel, iterations=1)
+    dilate = cv2.dilate(red_frame, kernel, iterations=1)
+    erode = cv2.erode(dilate, kernel, iterations=1)
+    cv2.imshow('Close', erode)
+
+    erode2 = cv2.erode(red_frame, kernel, iterations=1)
+    dilate2 = cv2.dilate(erode2, kernel, iterations=1)
+    cv2.imshow('Open', dilate2)
 
 
-    moment = cv2.moments(erode)
+    moment = cv2.moments(dilate2)
+
     if (moment["m00"] != 0):
         print(f"Площадь: {moment['m00']}")
         print(f"Моменты 1 порядка: {moment['m01']}, {moment['m10']}")
-        xc = int(moment['m10'] / moment['m00'])
-        yc = int(moment['m01'] / moment['m00'])
-    (contours, _) = cv2.findContours(erode.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for countour in contours:
-        (x, y, w, h) = cv2.boundingRect(countour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+        x = int(moment['m10'] / moment['m00'])
+        y = int(moment['m01'] / moment['m00'])
+        w = int(np.sqrt(moment['m00']))
+        h = int((moment['m00'] / w))
+        cv2.rectangle(frame, (x - (w//2), y - (h//2)), (x + (w//2), y + (h//2)), (255, 0, 0), 4)
 
-    cv2.imshow('Rec', frame)
+    cv2.imshow('Moments', frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
