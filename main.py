69849f8f8dc15a7d309e9d9a9a7eb60d94883949
efftea <daanikwork@mail.ru>
2024-11-14 23:27:02 +0300

    Сделана ЛР4.

--- main.py
+++ main.py
@@ -1,52 +1,92 @@
 import numpy as np
 import cv2
 
-def gauss(x,y,a,b,om):
-    return (1/(2*np.pi*(om**2))) * np.exp(-((((x-a)**2)+((y-b)**2))/(2*(om**2))))
+def kanny(gauss):
+    border = np.zeros(gauss.shape)
+    lengthVec = np.zeros(gauss.shape)
+    color = np.zeros(gauss.shape)
+    maxLen = np.max(lengthVec)
+    x = 0
+    y = 0
+    for i in range(1, len(gauss)-1):
+        for j in range(1, len(gauss[0])-1):
+            GrX = (gauss[i + 1][j + 1] - gauss[i - 1][j - 1] + gauss[i + 1][j - 1] - gauss[i - 1][j + 1] + 2 * (gauss[i + 1][j] - gauss[i - 1][j]))
+            GrY = (gauss[i + 1][j + 1] - gauss[i - 1][j - 1] + gauss[i - 1][j + 1] - gauss[i + 1][j - 1] + 2 * (gauss[i][j + 1] - gauss[i][j - 1]))
+            lengthVec[i][j] = np.sqrt(GrX ** 2 + GrY ** 2)
+            tg = np.arctan(GrY / GrX)
 
-def blur(blurImg, size, om):
-    kernelMatrix = np.empty((size, size))
-    a = (size)//2
-    b = (size)//2
-    sum = 0
-    for i in range(size):
-        for j in range(size):
-            kernelMatrix[i][j] = gauss(i,j,a,b,om)
-            sum += kernelMatrix[i][j]
+            if (GrX > 0 and GrY < 0 and tg < -2.414) or (GrX < 0 and GrY < 0 and tg > 2.414):
+                color[i][j] = 0
+                x = 0
+                y = -1
+            elif (GrX > 0 and GrY < 0 and tg < -0.414):
+                color[i][j] = 1
+                x = 1
+                y = -1
+            elif (GrX > 0 and GrY < 0 and tg > -0.414) or (GrX > 0 and GrY > 0 and tg > 0.414):
+                color[i][j] = 2
+                x = 1
+                y = 0
+            elif (GrX > 0 and GrY > 0 and tg < 2.414):
+                color[i][j] = 3
+                x = 1
+                y = 1
+            elif (GrX > 0 and GrY > 0 and tg > 2.414) or (GrX < 0 and GrY > 0 and tg < -2.414):
+                color[i][j] = 4
+                x = 0
+                y = 1
+            elif (GrX < 0 and GrY > 0 and tg < -0.414):
+                color[i][j] = 5
+                x = -1
+                y = 1
+            elif (GrX < 0 and GrY > 0 and tg > -0.414) or (GrX < 0 and GrY < 0 and tg < 0.414):
+                color[i][j] = 6
+                x = -1
+                y = 0
+            elif (GrX < 0 and GrY < 0 and tg < 2.414):
+                color[i][j] = 7
+                x = -1
+                y = -1
 
-    print(kernelMatrix)
-    print(sum)
-    kernelMatrix /= sum
-    print(kernelMatrix)
-    for i in range(size):
-        for j in range(size):
-            sum += kernelMatrix[i][j]
-    print(sum)
+            if (lengthVec[i][j]>lengthVec[i+x][j+y] and lengthVec[i][j]>lengthVec[i-x][j-y]):
+                border[i][j] = 255
+            else:
+                border[i][j] = 0
 
-    n_img = blurImg.copy()
+    cv2.imshow("lengths", lengthVec)
+    cv2.imshow("color", color)
+    cv2.imshow("border", border)
 
-    for i in range(0, blurImg.shape[0]):
-        for j in range(0,  blurImg.shape[1]):
-            val=0
-            for k in range(i if (blurImg.shape[0]-i >= size) else i-(size-((blurImg.shape[0]-i)%size)), (i+size) if (blurImg.shape[0]-i >= size) else i+((blurImg.shape[0]-i)%size)):
-                for l in range(j if (blurImg.shape[1]-j >= size) else j-(size-((blurImg.shape[1]-j)%size)), (j+size) if (blurImg.shape[1]-j >= size) else j+((blurImg.shape[1]-j)%size)):
-                    val += blurImg[k,l] * kernelMatrix[(k-i) if (blurImg.shape[0]-i >= size) else (k - (i-(size-((blurImg.shape[0]-i)%size)))), (l - j) if (blurImg.shape[1]-j >= size) else (l - (j-(size-((blurImg.shape[1]-j)%size))))]
-                    # print(f"{i}, {j}, {k}, {l}")
-            n_img[i, j] = val
+    low_level = maxLen // 25
+    high_level = maxLen // 10
+    for x in range(1, (len(gauss) - 1)):
+        for y in range(1, len(gauss[0]) - 1):
+            if (border[x][y] == 255):
+                if (lengthVec[x][y] < low_level):
+                    border[x][y] = 0
 
+            if (border[x][y] == 255):
+                if (lengthVec[x][y] <= high_level):
+                    if (border[x - 1][y - 1] == 255 or border[x - 1][y] == 255 or border[x - 1][y + 1] == 255 or border[x][y + 1] == 255 or border[x + 1][y + 1] == 255 or border[x + 1][y] == 255 or border[x + 1][y - 1] == 255 or border[x][y - 1] == 255):
+                        border[x][y] = 255
+                    else:
+                        border[x][y] = 0
 
-    return n_img
+    cv2.imshow("borders filtered", border)
 
-img = cv2.imread("2375-202111021051505426.png",cv2.IMREAD_GRAYSCALE)
-img1 = cv2.imread("2375-202111021051505426.png",cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("9349507864_4e82203bf8_b.jpg",cv2.IMREAD_GRAYSCALE)
+
 img = cv2.resize(img, (480, 600))
-img1 = cv2.resize(img1, (480, 600))
-size = int(input())
-om = int(input())
-cv2.imshow("not_gray", img)
-cv2.imshow("not_blured", img1)
-cv2.imshow("blured", blur(img1, size, om))
-cv2.imshow("blured2", cv2.GaussianBlur(img1,(size,size),om))
+
+size = int(input("Gauss Size: "))
+om = int(input("Gauss Sigma: "))
+
+gauss = cv2.GaussianBlur(img, (size, size), om)
+
+kanny(gauss)
+
+cv2.imshow("gray", img)
+cv2.imshow("blured", gauss)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
