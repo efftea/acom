4fd8dbcb008c34506d60f9cea161fc0caad0465b
efftea <131270184+efftea@users.noreply.github.com>
2024-12-06 03:50:59 +0300

    Сделана ИЗ1.

---
+++ MedianFlow.py
@@ -0,0 +1,51 @@
+import time
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('./Видосы/5.mp4')
+fourcc = cv2.VideoWriter.fourcc(*'XVID')
+out = cv2.VideoWriter('./Result/MedianFlow/5_MedianFlow.avi', fourcc, 20.0, (1280, 720))
+
+start = time.time()
+
+ret, frame = cap.read()
+
+frame = cv2.resize(frame, (1280, 720))
+
+
+bbox = cv2.selectROI("Tracking", frame, fromCenter=False, showCrosshair=True)
+
+tracker = cv2.legacy.TrackerMedianFlow_create()
+tracker.init(frame, bbox)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.resize(frame, (1280, 720))
+
+    success, bbox = tracker.update(frame)
+
+    if success:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, "Tracking Success", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    else:
+        cv2.putText(frame, "Tracking Failure", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+    out.write(frame)
+    cv2.imshow('TrackMedianFlow', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+end = time.time()
+
+if cap.get(cv2.CAP_PROP_FRAME_COUNT) != 0:
+    print(f"Время обработки одного кадра MedianFlow: {end - start:.5f} секунд")
+    print(f"Частота кадров: {cap.get(cv2.CAP_PROP_FPS):.0f} кадров в секунду")
+    print(f"Частота обработки объектов: {1 / ((end - start) / cap.get(cv2.CAP_PROP_POS_FRAMES)):.0f} кадров в секунду")
+else:
+    print("Видеопоток был пуст")
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
