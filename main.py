6264c3113ba2a852f9293d47c0ebc60dd9a46926
efftea <131270184+efftea@users.noreply.github.com>
2024-12-13 02:37:47 +0300

    Сделана ЛР6.

---
+++ main.py
@@ -0,0 +1,6 @@
+import keras
+from keras._tf_keras.keras.datasets import mnist
+from keras._tf_keras.keras.layers import Dense
+from keras._tf_keras.keras.utils import to_categorical
+from keras._tf_keras.keras.optimizers import Adam
+
